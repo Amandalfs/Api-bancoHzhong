8e106906d029950159ac321f1fa3b0af206e5185
Amandalfs <caduc57@gmail.com>
2023-03-15 15:28:30 -0300

    feat
    adicionado aysnc e await no depositUsers

--- routes/depositUsers.js
+++ routes/depositUsers.js
@@ -21,21 +21,25 @@ const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringify(users, nul
 
 const depositUsers = (app) => {
     app.route('/depositUsers')
-        .patch((req, res)=>{
-            const users = getUsers()
-            let valueBoolean = true;
-            users.map((user, index, array)=>{
-                if(user.nameUser===req.body.nameUser && user.senha1 === req.headers.password){
-                    const deposit = Number(req.body.deposit);
-                    users[index].balance += deposit;
-                    users[index].history.push(`Depositou R$${deposit.toFixed(2).replace('.',',')} em ${generatorDate()}`)
-                    saveUser(users);
-                    valueBoolean = false;
-                    res.status(200).send('OK');
+        .patch(async(req, res)=>{
+            try{
+                const users = await getUsers()
+                let valueBoolean = true;
+                await users.map((user, index, array)=>{
+                    if(user.nameUser===req.body.nameUser && user.senha1 === req.headers.password){
+                        const deposit = Number(req.body.deposit);
+                        users[index].balance += deposit;
+                        users[index].history.push(`Depositou R$${deposit.toFixed(2).replace('.',',')} em ${generatorDate()}`)
+                        saveUser(users);
+                        valueBoolean = false;
+                        res.status(200).send('OK');
+                    }
+                })
+                if(valueBoolean){
+                    res.status(400).send("Error Deposit");
                 }
-            })
-            if(valueBoolean){
-                res.status(400).send("Error Deposit");
+            }catch(error){
+                console.log(error);
             }
         })
 }
