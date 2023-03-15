b67f22cdbd6a6c75a46464d378c412798bdb57c5
Amandalfs <caduc57@gmail.com>
2023-03-15 15:54:06 -0300

    feat
    adicionado async e await no pix  key transaction

--- routes/pixKeyTransaction.js
+++ routes/pixKeyTransaction.js
@@ -21,42 +21,49 @@ const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringify(users, nul
 
 const pixKeyTransaction = (app) =>{
     app.route('/pixKeyTransaction')
-        .patch((req, res)=>{
-            const users = getUsers();
-            let valueBoolean = true;
-            let valueBoolean2 = true;
+        .patch(async(req, res)=>{
+            try{
+                const users = await getUsers();
+                let valueBoolean = true;
+                let valueBoolean2 = true;
 
-            users.map((userSend, indexSend, arraySend)=>{
-                const depositSend = Number(req.body.deposit);
-                if(userSend.nameUser === req.body.nameUser && userSend.senha1 === req.headers.password){
-                    valueBoolean = false;
-                    if(userSend.balance>depositSend){
-                        valueBoolean2 = false;
-                        users.map((userReceive, indexReceive, arrayReceive)=>{
-                            if(userReceive.keyPix === req.body.keyPix){
-                                users[indexSend].balance -= depositSend;
-                                users[indexSend].history.push(`Voce transferiu R$${depositSend.toFixed(2).replace('.',',')} para @${userReceive.nameUser} ${dateGenerator()}`);
-                                users[indexReceive].balance += depositSend;
-                                users[indexReceive].history.push(`Voce recebeu R${depositSend.toFixed(2).replace('.',',')} de @${userSend.nameUser} ${dateGenerator()}`);
-                                saveUser(users);
-                                return res.status(200).send(userSend.history[userSend.history.length-1]);
-                            }
-                        })
-                        return res.status(400).send('Error KeyPix invalidate');
-                    } 
+                await users.map(async(userSend, indexSend, arraySend)=>{
+                    try{
+                        const depositSend = await Number(req.body.deposit);
+                        if(userSend.nameUser === req.body.nameUser && userSend.senha1 === req.headers.password){
+                            valueBoolean = false;
+                            if(userSend.balance>depositSend){
+                                valueBoolean2 = false;
+                                await users.map((userReceive, indexReceive, arrayReceive)=>{
+                                    if(userReceive.keyPix === req.body.keyPix){
+                                        users[indexSend].balance -= depositSend;
+                                        users[indexSend].history.push(`Voce transferiu R$${depositSend.toFixed(2).replace('.',',')} para @${userReceive.nameUser} ${dateGenerator()}`);
+                                        users[indexReceive].balance += depositSend;
+                                        users[indexReceive].history.push(`Voce recebeu R${depositSend.toFixed(2).replace('.',',')} de @${userSend.nameUser} ${dateGenerator()}`);
+                                        saveUser(users);
+                                        return res.status(200).send(userSend.history[userSend.history.length-1]);
+                                    }
+                                })
+                                return res.status(400).send('Error KeyPix invalidate');
+                            } 
 
+                        }
+                    }catch(erro2){
+                        console.log(erro2)
+                    }
+
+                })
+
+                if(valueBoolean){
+                    return res.status(400).send('account does not exist')
                 }
-
-            })
-
-            if(valueBoolean){
-                return res.status(400).send('account does not exist')
+                
+                if(valueBoolean2){
+                    return res.status(400).send('insufficient fund')
+                }
+            }catch(error){
+                console.log(error);
             }
-            
-            if(valueBoolean2){
-                return res.status(400).send('insufficient fund')
-            }
-
         })
 }
 
