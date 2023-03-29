303188765085cdb3432b2ed4b59a54dced0006e8
Amandalfs <caduc57@gmail.com>
2023-03-29 12:12:02 -0300

    feat
    Adicionando a keypix no banco de dados

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const loginUser = require('./routes/loginUser');
 const withdrawUser = require('./routes/withdrawUser');
 const depositUser = require('./routes/depositUser');
 const extractUser = require('./routes/extractUser');
-const createKeyPixUsers = require('./routes/createKeyPixUsers');
+const createKeyPixUser = require('./routes/createKeyPixUser');
 const deleteKeyPixUsers = require('./routes/deleteKeyPixUsers');
 const pixKeyTransaction = require('./routes/pixKeyTransaction');
 
@@ -19,9 +19,9 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-pixKeyTransaction(app);
-deleteKeyPixUsers(app);
-createKeyPixUsers(app);
+pixKeyTransaction(app); //
+deleteKeyPixUsers(app); //
+createKeyPixUser(app); //
 extractUser(app);
 depositUser(app);
 createUser(app);
---
+++ routes/createKeyPixUser.js
@@ -0,0 +1,35 @@
+const { patch } = require('http');
+const { join } = require('path');
+const keyGenerator = require('../utils/keyGenerator')
+const pool = require('../sql/sqlconfig');
+
+async function selectAll(){
+    const dados = await pool.query('SELECT * FROM "dadosbanco"');
+    return dados.rows;
+}
+
+
+const createKeyPixUser = (app) =>{
+    app.route('/createKeyPixUser')
+        .patch(async(req, res) =>{
+            const users = await selectAll();   
+            let valueBoolean = await true;
+
+            await users.map(async(user, index, array)=>{
+                if(req.body.username === user.username && req.headers.password === user.password && user.keypix === null){
+                    const sqlKey = ('UPDATE dadosbanco SET keypix=$1 WHERE username like $2');
+                    const valuesKey = [`${keyGenerator()}`, user.username];
+                    const key = valuesKey[0];
+                    pool.query(sqlKey, valuesKey);
+    
+                    valueBoolean = false;
+                    return res.status(201).send({key});
+                }
+            })
+            if(valueBoolean){
+                return res.status(400).send('Error create KEYPIX/existing key')
+            }
+        })
+}
+
+module.exports = createKeyPixUser;
