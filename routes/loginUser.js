a62938efa9568d8fc54b4619bf0cdee9ea7cbc6d
Amandalfs <caduc57@gmail.com>
2023-04-05 17:03:23 -0300

    patch
    Mudada o retorno da rota login para so enviar o username

--- routes/loginUser.js
+++ routes/loginUser.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const { get } = require('http');
 const pool = require('../sql/sqlconfig');
-const periodMonth = require('../utils/periodMonth')
 const selectAll = require('../utils/selectAll');
 
 const loginUser = (app) => {
@@ -9,19 +8,11 @@ const loginUser = (app) => {
         .get(async(req, res) => {
             let value = await true;
             const users = await selectAll();
-            console.log(req.query);
-            /* console.log(req.body['username'], req.headers['password']);
-            console.log(users); */
             await users.map(async(user) => {
                 if(user.username === req.query.username && user.password ===  req.query.password){
                     value = false;
-                    // format de date select * from extrato where date = '2023-03-29'
-                    const sql = await('SELECT * from extrato WHERE username like $1 AND date > $2 LIMIT 5');
-                    const extrato = await pool.query(sql, [user.username, periodMonth()]);
                     const userData = {
                         "username": user.username,
-                        "Saldo": user.saldo,
-                        "extrato": extrato.rows 
                     }
                     res.status(200).send({userData});
                 }
---
+++ routes/users/userAconnt.js
@@ -0,0 +1,3 @@
+/* const sql = await('SELECT * from extrato WHERE username like $1 AND date > $2 LIMIT 5');
+const extrato = await pool.query(sql, [user.username, periodMonth()]); */
+// format de date select * from extrato where date = '2023-03-29'
