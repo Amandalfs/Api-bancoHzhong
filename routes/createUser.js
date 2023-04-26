30f1ec09b61b5034bb704bd44d23228378ce31d6
Amandalfs <caduc57@gmail.com>
2023-04-26 07:42:38 -0300

    feat
    transferido a rota de pg bruto para a orm knex e add mais organizacao nas logicas do codigo

--- routes/createUser.js
+++ routes/createUser.js
@@ -1,20 +1,13 @@
-const { post } = require('http');
-const { join } = require('path');
-
-const verifyAge = require('../utils/verifyAge');
-
-const { pool } = require('../sql/sqlconfig');
-const selectAll = require('../utils/selectAll');
+const { db } = require('../sql/sqlconfig');
 
 const dbUsers = require('../sql/dbUsers');
 
 
-
 const validarCPF = (req, res, next)=>{
-    const checkSizeCpf = require('../utils/checkSizeCpf');
-    const verifyCpf = require('../utils/verifyCpf');
+    const checkSizeCpf = require('../utils/verify/checkSizeCpf');
+    const verifyCpf = require('../utils/verify/verifyCpf');
 
-    const {cpf} = req.body;
+    const { cpf } = req.headers;
 
     const errors = [];
     if(verifyCpf(cpf)){
@@ -35,29 +28,118 @@ const validarCPF = (req, res, next)=>{
 
 }   
 
+const isEmailUsernameCpf = async (req, res, next)=>{
+    const db = require('../sql/knex/index')
+
+    const { email, username} = req.body;
+    const { cpf } = req.headers;
+    const errors = [];
+
+    const isEmail = await db('users').where("email", email).select()
+    const isUsername = await db('users').where("username", username).select()
+    const isCpf = await db('users').where("cpf", cpf).select()
+
+    if(isEmail.length!==0){
+        errors.push("Ja existente uma conta com esse Email")
+    }
+
+    if(isUsername.length!==0){
+        errors.push("Ja existente uma conta com esse CPF")
+    }
+
+    if(isCpf.length!==0){
+        errors.push("Ja existente uma conta com esse Username")
+    }
+
+    if(errors.length!==0){
+        return res.status(401).send({errors})
+    }
+
+    next();
+}
+
+const verificarSenha = (req, res, next)=>{
+    const {password2, password } = req.headers;
+
+    if(password !== password2){
+        console.log(password, password2)
+        return res.status(400).send("senhas diferentes");
+    }
+
+    next();
+}
+
+const isMaior = (req, res, next) =>{
+    const verifyAge = require('../utils/verify/verifyAge');
+
+    const { nasc } = req.body;
+
+    if(verifyAge(nasc)){
+        return res.status(401).send("Usuario e menor de idade");
+    }
+
+    next();
+}
+
+const validarDados = (req, res, next) =>{
+    const {username, name, nasc, typeaccont, email} = req.body;
+
+    const errors = [];
+
+    if(!username){
+        errors.push("campo username nao foi preenchido")
+    }
+    
+    if(!name){
+        errors.push("campo nome nao foi preenchido")
+    }
+
+    if(!nasc){
+        errors.push("campo nascimento nao foi preenchido")
+    }
+
+    if(!typeaccont){
+        errors.push("campo tipo nao foi preenchido")
+    }
+
+    if(!email){
+        errors.push("campo email nao foi preenchido")
+    }
+
+    if(errors.length!==0){
+        return res.status(400).res({errors})
+    }
+
+
+    next();
+}
+
 const createUser = (app) => {
     app.route('/createUser')
-        .post(validarCPF,async (req, res) => {
-            const users = await selectAll();
-            let value = true;
-            if(verifyAge(req.body.nasc)||( req.headers.password !== req.headers.password2)){
-                value = false;
-                return res.status(400).send("error create account")
+        .post(validarCPF, isEmailUsernameCpf, verificarSenha, isMaior, validarDados, async (req, res) => {
+            const {username, name, nasc, typeaccont, email} = req.body;
+            const { password, cpf } = req.headers
+            
+            const infosDefault = {
+                numero: 153,
+                agencia: "003",
+                saldo: 0            
             }
-            users.map((user) => {
-                if(user.email === req.body.email || user.cpf === req.body.cpf || user.username === req.body.username) {
-                    value = false;
-                    return res.status(400).send("error cpf or email invalid");
-                }
-            })
-
-            if(value){
-                pool.connect()
-                const sql = 'INSERT INTO dadosbanco(name, username, password, nasc, typeaccont, numero, agencia, saldo, email, cpf) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);';
-                const values = [req.body.name, req.body.username, req.headers.password, req.body.nasc, req.body.typeaccont, 153, '003', 0, req.body.email, req.body.cpf];
-                pool.query(sql, values);
-                res.status(201).send('Create Accont Success');
+            const userNew = {
+                ...infosDefault,
+                username: username,
+                name: name,
+                nasc: nasc, 
+                typeaccont: typeaccont,
+                email: email,
+                password: password,
+                cpf: cpf,
             }
+            await dbUsers.createUser(userNew);
+            return res.status(201).send('Conta no hzhong criada com sucesso');
+    
         })
 }
+
 module.exports = createUser;
+
