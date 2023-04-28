dda5cda9d897c80a812cc8d8e21d507a0691e41b
Amandalfs <caduc57@gmail.com>
2023-04-28 01:16:55 -0300

    feat
    adicionado a lib cryto para gerar a key

--- utils/keyGenerator.js
+++ utils/keyGenerator.js
@@ -1,75 +1,23 @@
-function randowLetterNumber(){
-    const numero = Math.ceil((Math.random())*35);
-    if(numero>9){
-        switch(numero-10){
-            case 0: 
-                return 'a'
-            case 1:
-                return 'b'
-            case 2:
-                return 'c'
-            case 3:
-                return 'd'
-            case 4: 
-                return 'e'
-            case 5:
-                return 'f'
-            case 6:
-                return 'g'
-            case 7:
-                return 'h'
-            case 8:
-                return 'i'
-            case 9:
-                return 'j'
-            case 10:
-                return 'k'
-            case 11:
-                return 'l'
-            case 12:
-                return 'm'
-            case 13:
-                return 'n'
-            case 14:
-                return 'o'
-            case 15:
-                return 'p'
-            case 16:
-                return 'q'
-            case 17: 
-                return 'r'
-            case 18:
-                return 's'
-            case 19:
-                return 't'
-            case 20: 
-                return 'u'
-            case 21:
-                return 'v'
-            case 22:
-                return 'w'
-            case 23:
-                return 'x'
-            case 24: 
-                return 'y'
-            case 25: 
-                return 'z'
+const db = require('../sql/knex/index');
+const crypto = require('crypto');
+
+async function keyGenerator(){
+    const key = crypto.randomUUID().replace(/-/g, '').substring(0, 32)
+    let keyFormatada = "";
+    for(let indice in key){
+        if(indice==8 || indice==16 || indice==24){
+            keyFormatada += "-"
         }
+        keyFormatada += key[indice];
+    }
+    const value = await db('users').where("keypix", keyFormatada);
+    if(value.length==0){
+        return keyFormatada
     } else {
-        return String(numero)
+        keyGenerator();
     }
 }
 
-function keyGenerator(){
-    let key = '';
-    for(let c=1; c<=32; c++){
-        if(c==8 || c==16 || c==24){
-            key += '-'
-        } else {
-            key += randowLetterNumber()
-        }
-    }
-    return key
-}
+keyGenerator()
 
 module.exports = keyGenerator;
