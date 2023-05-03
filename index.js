50d162af73738ccd32d4a63893ff3307e27e46be
Amandalfs <caduc57@gmail.com>
2023-05-03 05:50:58 -0300

    feat
    configurado o servidor para qualquer erro que dar nao derrubar a aplicacao

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('express-async-errors')
+
 const express =  require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')
-const routes = require('./routes')
+const cors = require('cors');
+const routes = require('./routes');
+
+const AppError = require('./utils/AppError');
 
 const userAccont = require('./routes/users/userAconnt');
 
@@ -10,12 +13,26 @@ const port = 8020;
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-
 
 app.use(routes);
 
 userAccont(app);
+
+app.use((error, req, res, next)=>{
+    if(error instanceof AppError){
+        return res.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    console.error(error);
+    
+    return res.status(500).json({
+        status: "Error",
+        message: "Internal Server Error"
+    })
+})
 
 app.get('/', (req, res) => res.send('funcionou'));
 
