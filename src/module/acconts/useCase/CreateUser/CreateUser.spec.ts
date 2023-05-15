5ba72395c6992dbc1aa64fcd5046de994a8f7aa9
Amandalfs <caduc57@gmail.com>
2023-05-15 19:52:59 -0300

    test
    teste da condicao de username ja existente

--- src/module/acconts/useCase/CreateUser/CreateUser.spec.ts
+++ src/module/acconts/useCase/CreateUser/CreateUser.spec.ts
@@ -66,5 +66,38 @@ describe("criacao de usuarios",()=>{
         server.close()
     })
         
-        
+    it("Deve nao pode cadastrar com um username ja existente", async()=>{
+        const server = app.listen(2000,()=>{});
+
+        await request(server)
+            .post("/users")
+            .send({
+                "username": "UsuarioTest",
+                "name": "Usuario Test",
+                "nasc": "02-10-2003",
+                "typeaccont": "poupanca",
+                "email": "usuario57@test.com",
+                "password": "12345678",
+                "password2": "12345678",
+                "cpf": "12603863096"
+            })
+
+        const result = await request(server)
+            .post("/users")
+            .send({
+                "username": "UsuarioTest",
+                "name": "Usuario Test2",
+                "nasc": "02-10-2003",
+                "typeaccont": "poupanca",
+                "email": "usuario58@test.com",
+                "password": "12345678",
+                "password2": "12345678",
+                "cpf": "56711478088"
+            })
+        const dados = JSON.parse(result.text)
+        console.log(dados.message)
+        expect(dados.message).toEqual("Ja existente uma conta com esse Username");
+        server.close()
+    })
+
 })
