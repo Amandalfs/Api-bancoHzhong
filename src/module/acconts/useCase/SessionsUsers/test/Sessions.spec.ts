071b72d37dbeb5aa71ca8ca5d516649e90bfd240
Amandalfs <caduc57@gmail.com>
2023-05-18 13:52:11 -0300

    refactor
    otimizacao dos testes de sessions

--- src/module/acconts/useCase/SessionsUsers/test/Sessions.spec.ts
+++ src/module/acconts/useCase/SessionsUsers/test/Sessions.spec.ts
@@ -1,77 +1,75 @@
-import { execSync } from "child_process";
 import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
-import { app } from "../../../../../app";
-import  request  from "supertest"
+import { InMemoryUsersRepository } from "../../../../../repositories/inMemory/InMemoryUsersRepository";
+import { SessionsUsersUseCase } from "../SessionsUsersUseCase";
+import { hash } from 'bcrypt';
+import { AppError } from "../../../../../utils/AppError";
 
 describe('Testando Login de usuario',()=>{
-    beforeEach(()=>{
-        execSync("npm run knex -- migrate:latest")
-    })
-
-    afterEach(()=>{
-        execSync("npm run knex --  migrate:rollback --all")
-    })
 
     it("usuario nao deve conseguir logar com um a senha errada", async()=>{
-        const server = app.listen();
-        await request(server)
-            .post("/users")
-            .send({
-                "username": "UsuarioTest",
-                "name": "Usuario Test",
-                "nasc": "02-10-2003",
-                "typeaccont": "poupanca",
-                "email": "usuario57@test.com",
-                "password": "12345678",
-                "password2": "12345678",
-                "cpf": "12603863096"
-            })
-        const response = await request(server)
-            .get("/users")
-            .send({
-                "username": "UsuarioTest",
-                "password": "4815415213"
-            })  
-        const result = JSON.parse(response.text)
-        console.log(result.message)
-        expect(result.message).toEqual("Senha Ou Username digitada esta errada")
-        server.close()
+        const usersRepository = new InMemoryUsersRepository;
+        const sessionsUsersUseCase = new SessionsUsersUseCase(usersRepository)
+        const senhaCriptografada = await hash("12345678", 8)
+
+        usersRepository.createUser({
+            numero: 153,
+            agencia: "003",
+            saldo: 0, 
+            "username": "UsuarioTest",
+            "name": "Usuario Test",
+            "nasc": "02-10-2003",
+            "typeaccont": "poupanca",
+            "email": "usuario57@test.com",
+            "password": senhaCriptografada,
+            "cpf": "12603863096"
+        })
+        await expect(sessionsUsersUseCase.execute("UsarioTest", "524545")).rejects.toEqual(new AppError("Senha Ou Username digitada esta errada", 401))
+
+        
+
     })
 
     it("Usuario nao pode logar com uma conta que nao existe", async ()=>{
-        const server = app.listen();
-        const response = await request(server)
-            .get("/users")
-            .send({
-                "username": "UsarioTest",
-                "password": "524545"
-            })
-        const result = JSON.parse(response.text)
-        expect(result.message).toEqual("Senha Ou Username digitada esta errada")
-        server.close()
+        const usersRepository = new InMemoryUsersRepository;
+        const sessionsUsersUseCase = new SessionsUsersUseCase(usersRepository)
+        const senhaCriptografada = await hash("12345678", 8)
+
+        usersRepository.createUser({
+            numero: 153,
+            agencia: "003",
+            saldo: 0, 
+            "username": "UsuarioTest",
+            "name": "Usuario Test",
+            "nasc": "02-10-2003",
+            "typeaccont": "poupanca",
+            "email": "usuario57@test.com",
+            "password": senhaCriptografada,
+            "cpf": "12603863096"
+        })
+        await expect(sessionsUsersUseCase.execute("Uggbfri0bnaeipbijrth", "12345678")).rejects.toEqual(new AppError("Senha Ou Username digitada esta errada", 401))
     })
 
     it("Usuario deve conseguir iniciar uma sessao com username e senha certa", async()=>{
-        const server = app.listen();
-        await request(server)
-            .post("/users")
-            .send({
-                "username": "UsuarioTest",
-                "name": "Usuario Test",
-                "nasc": "02-10-2003",
-                "typeaccont": "poupanca",
-                "email": "usuario57@test.com",
-                "password": "12345678",
-                "password2": "12345678",
-                "cpf": "12603863096"
-            })
-        await request(server)
-            .get("/users")
-            .send({
-                "username": "UsuarioTest",
-                "password": "12345678"
-            })
-        .expect(202)  
-        server.close()
+        const usersRepository = new InMemoryUsersRepository;
+        const sessionsUsersUseCase = new SessionsUsersUseCase(usersRepository)
+        const senhaCriptografada = await hash("12345678", 8)
+
+        usersRepository.createUser({
+            numero: 153,
+            agencia: "003",
+            saldo: 0, 
+            "username": "UsuarioTest",
+            "name": "Usuario Test",
+            "nasc": "02-10-2003",
+            "typeaccont": "poupanca",
+            "email": "usuario57@test.com",
+            "password": senhaCriptografada,
+            "cpf": "12603863096"
+        })
+
+        const token = await sessionsUsersUseCase.execute("UsuarioTest", "12345678")
+
+        expect(token).toEqual(expect.any(String))
+
     })
 })
--- src/module/acconts/useCase/SessionsUsers/test/testes.md
+++ src/module/acconts/useCase/SessionsUsers/test/testes.md
@@ -3,3 +3,7 @@
 - [x] Usuario nao pode logar com a senha errada.
 - [x] Nao pode logar com um username que nao existe conta.
 - [x] Usuario deve poder logar com a senha e username correto.
+
+# Atualizacoes Do dia 18/05/2023
+
+- [x] Otimizacao dos Testes de Sessions
