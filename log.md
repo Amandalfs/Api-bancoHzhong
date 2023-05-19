cb1c2c2a276bf1e494bb069f80385958d208089c
Amandalfs <caduc57@gmail.com>
2023-05-19 12:25:26 -0300

    test
    Testes showUsers Concluido

--- log.md
+++ log.md
@@ -3,3 +3,4 @@
 # 19/05/2023 
 
 - [x] Criacao do Repository InMemory do extracts
+- [X] Criacao dos testes do UseCase ShowUsers e Testes Executados com tudo certo
--- src/module/acconts/useCase/ShowUser/test/ShowUsers.spec.ts
+++ src/module/acconts/useCase/ShowUser/test/ShowUsers.spec.ts
@@ -3,6 +3,7 @@ import { InMemoryExtractsRepository } from "../../../../../repositories/inMemory
 import { ShowUserUseCase } from "../ShowUserUseCase";
 import { InMemoryUsersRepository } from "../../../../../repositories/inMemory/InMemoryUsersRepository";
 import { AppError } from "../../../../../utils/AppError";
+import { hash } from 'bcrypt';
 
 
 describe("Testando a funcionalidade show users", ()=>{
@@ -12,5 +13,29 @@ describe("Testando a funcionalidade show users", ()=>{
         const extractsRepository = new InMemoryExtractsRepository;
         const showUserUseCase = new ShowUserUseCase(usersRepository, extractsRepository);
         await expect(showUserUseCase.execute(515)).rejects.toEqual(new AppError("Usuario nao encontrado"))
+    })
+
+    
+    it("Usuario deve conseguir usar show com uma conta existente", async ()=>{
+        const usersRepository = new InMemoryUsersRepository;
+        const extractsRepository = new InMemoryExtractsRepository;
+
+        const senhaCriptografada = await hash("12345678", 8)
+        await usersRepository.createUser({
+            numero: 153,
+            agencia: "003",
+            saldo: 0, 
+            "username": "UsuarioTest",
+            "name": "Usuario Test",
+            "nasc": "02-10-2003",
+            "typeaccont": "poupanca",
+            "email": "usuario57@test.com",
+            "password": senhaCriptografada,
+            "cpf": "12603863096"
+        })
+
+        const showUserUseCase = new ShowUserUseCase(usersRepository, extractsRepository);
+        const response = await showUserUseCase.execute(1)
+        expect(response).toEqual(expect.any(Object))
     })
 })
