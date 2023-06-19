f8491d679232de602f9aed4b304a81a80256a36d
Amandalfs <amandalfs57@gmail.com>
2023-06-19 11:18:36 -0300

    test: Criado o tratamento de erro 400 no controlador CreateUseCase

--- src/presentations/controllers/users/CreateUser/CreateUserController.spec.ts
+++ src/presentations/controllers/users/CreateUser/CreateUserController.spec.ts
@@ -3,6 +3,7 @@ import { HttpController } from './../../../protocols/Controller';
 import { HttpRequest } from './../../../protocols/http'
 import { CreateUserController } from "./CreateUserController";
 import { ICreateUserRequestDTO, ICreateUserResponseDTO, ICreateUserUseCase } from "../../../../module/acconts/useCase/CreateUser/CreateUserUseCase";
+import { BadRequest } from "../../../helpers";
 
 interface TypesSut {
     sut,
@@ -36,11 +37,40 @@ describe("Testando o controllador de criacao de conta", ()=>{
                 nasc: "500", 
                 typeaccont: "500", 
                 email: "500"
+            },
+            headers: {
+                password: "500",
+                password2: "500", 
+                cpf: "500"
             }
         }
         const response = await sut.handle(request);
         expect(response.statusCode).toEqual(500);
         expect(response.body.msg).toEqual("Server Internal Error");
+    })
+
+    it("esperado que receba o controller consiga tratar erros de 400",async ()=>{
+        const { sut, useCase } = makeSut();
+        vi.spyOn(useCase, "execute").mockRejectedValue({statusCode: 400, message: "Bad Request"});
+
+        const request: HttpRequest = {
+            body: {
+                username: "Error400",
+                name: "Error400Name", 
+                nasc: "400", 
+                typeaccont: "400", 
+                email: "400"
+            },
+            headers: {
+                password: "400",
+                password2: "400", 
+                cpf: "400"
+            }
+        }
+        const response = await sut.handle(request);
+        console.log(response)
+        expect(response.statusCode).toEqual(400);
+        expect(response.body.msg).toEqual("Bad Request");
     })
 
 })
