ccea29c16ee440276ea1cd92785b2d4ff834b4b5
Amandalfs <amandalfs57@gmail.com>
2023-06-26 08:46:11 -0300

    test: Criado o controlador independente SendingMoneyTransactions e criado o tratamento de erros desconhecidos.

---
+++ src/presentations/controllers/transactions/SendingMoneyTransactions/SendingMoneyTransactionsController.spec.ts
@@ -0,0 +1,65 @@
+import { describe, expect, it, vi } from "vitest";
+import { HttpRequest } from "../../../protocols/http"
+import { DTORequestSendingMoneyUseCase, DTOResponseSendingMoneyUseCase, ISendingMoneyUseCase } from "../../../../module/transactions/useCase/SendingMoney/SendingMoneyUseCase";
+import { SendingMoneyTransactionsController } from "./SendingMoneyTransactionsController";
+
+interface TypesSut {
+    sut: SendingMoneyTransactionsController,
+    useCase: ISendingMoneyUseCase,
+}
+
+const makeSut = ():TypesSut =>{
+    const useCaseShowMock = new class sendingMoneyTransactionsUserUseCase implements ISendingMoneyUseCase {
+        execute(data: DTORequestSendingMoneyUseCase): Promise<DTOResponseSendingMoneyUseCase> {
+
+            const send = {
+                    id_user: 1,
+                    name: "Test 01",
+                    tipo: "envio",
+                    saldo: 50,
+                    data: `00000`,
+                    descricao: `Voce transferiu R$50`,
+            }
+
+            const receive = {
+                    id_user: 2,
+                    name: "Test 02",
+                    tipo: "recebido",
+                    saldo: 50,
+                    data: `00000`,
+                    descricao: `Voce recebeu R$50`,
+            }
+            
+            return new Promise(resolve => resolve(
+                new DTOResponseSendingMoneyUseCase(send, receive)
+            ))
+        }
+       
+    }
+
+    const sut = new SendingMoneyTransactionsController(useCaseShowMock);
+    
+    return {
+        sut,
+        useCase: useCaseShowMock,
+    }
+}
+
+describe("Testando o controllador de envio de dinheiro", ()=>{
+    it("esperado que receba o controller consiga tratar erros desconhecido",async ()=>{
+        const { sut, useCase } = makeSut();
+        vi.spyOn(useCase, "execute").mockRejectedValue(new Error);
+        const request: HttpRequest = {
+            user: {
+                id: 500,    
+            },
+            body: {
+                keypix: "500",
+                value: 500
+            }
+        }
+        const response = await sut.handle(request);
+        expect(response.statusCode).toEqual(500);
+        expect(response.body.msg).toEqual("Server Internal Error");
+    })
+})
