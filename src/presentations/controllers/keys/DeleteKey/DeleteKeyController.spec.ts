c9d90d2439abe2c73f3ce0142d7df61ce5c0dcec
Amandalfs <amandalfs57@gmail.com>
2023-06-29 12:05:16 -0300

    test: Criado o tratamento de erro 400 no controlador DeleteKey.

--- src/presentations/controllers/keys/DeleteKey/DeleteKeyController.spec.ts
+++ src/presentations/controllers/keys/DeleteKey/DeleteKeyController.spec.ts
@@ -39,5 +39,19 @@ describe("Testando o controlador de deleta chave", ()=>{
         expect(response.body.msg).toEqual("Server Internal Error");
     })
 
+    it("esperado que receba o controller consiga tratar erros 400",async ()=>{
+        const { sut, useCase } = makeSut();
+        vi.spyOn(useCase, "execute").mockRejectedValue({statusCode: 400, message: "Bad Request"});
+        const request: HttpRequest = {
+            user: {
+                id: 400,    
+            }
+        }
+        const response = await sut.handle(request);
+        expect(response.statusCode).toEqual(400);
+        expect(response.body.msg).toEqual("Bad Request");
+    })
+    
+
     
 })
