48024b1974625a59a626572343a6a6e3ad023d11
Amandalfs <amandalfs57@gmail.com>
2023-06-30 16:30:51 -0300

    fix: arrumado os scripts de teste

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		environmentMatchGlobs:[["src/main/controllers/*.e2e.spec.ts", "knex"]],	
+		environmentMatchGlobs:[["src/**", "knex"]],	
+		exclude:['node_modules', 'dist', '.idea', '.git', '.cache', 'src/main/**']
 	}
 });
