93c68f13c4997e929734685e44b704ee6889a1f7
Amandalfs <amandalfs57@gmail.com>
2023-07-12 13:37:23 -0300

    feat: criado a fabrica de controlador de delete key

--- src/domain/module/keys/useCase/DeleteKey/index.ts
+++
@@ -1,9 +0,0 @@
-import {UserRepository} from '../../../../../entities/repositories/knexRepositories/UserRepository';
-import {DeleteKeyUseCase} from './DeleteKeyUseCase';
-import {DeleteKeyController} from './DeleteKeyController';
-
-const userRepository = new UserRepository;
-const deleteKeyUseCase = new DeleteKeyUseCase(userRepository);
-const deleteKeyController = new DeleteKeyController(deleteKeyUseCase);
-
-export default deleteKeyController;
---
+++ src/main/controllers/factores/makeKeys/makeDeleteKeyController.ts
@@ -0,0 +1,10 @@
+import { DeleteKeyUseCase } from "../../../../domain/module/keys/useCase/DeleteKey/DeleteKeyUseCase";
+import { UserRepository } from "../../../../entities/repositories/knexRepositories/UserRepository";
+import { DeleteKeyController } from "../../../../presentations/controllers/keys/DeleteKey/DeleteKeyController";
+
+export function makeDeleteKeyController(){
+    const userRepository = new UserRepository;
+    const deleteKeyUseCase = new DeleteKeyUseCase(userRepository);
+    const controller = new DeleteKeyController(deleteKeyUseCase);
+    return controller;
+}
