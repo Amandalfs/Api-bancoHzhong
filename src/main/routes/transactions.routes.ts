e33a4530888c04514da18402d2899dac083db468
Amandalfs <amandalfs57@gmail.com>
2023-07-13 12:09:39 -0300

    feat: adicionado o controlador de deposit transactions nas rotas.

--- src/main/routes/transactions.routes.ts
+++ src/main/routes/transactions.routes.ts
@@ -1,49 +1,20 @@
 import garantirAuth from "../../middlewares/garantirAuth";
 
-import { NextFunction, Request, Response, Router } from "express";
+import { Request, Response, Router } from "express";
 const transactionsRoutes = Router();
 
-import { AppError } from "../../utils/errors/AppError";
-
-import depositTransactionsController from '../../domain/module/transactions/useCase/DepositTransactions';
 import withdrawTransactionsController from '../../domain/module/transactions/useCase/WithdrawTransactions';
 import sendingMoneyController from '../../domain/module/transactions/useCase/SendingMoney';
 import extractsByDataController from '../../domain/module/transactions/useCase/ExtractsByData';
 
-const validarDate = (req: Request, res: Response, next: NextFunction) =>{
-    const {dateInicial, dateFinal} = req.query;
+import { makeDepositTransactionsController } from "../controllers/factores/makeTransactions/makeDepositTransactionsControlller";
 
-    let pattern = /^\d{4}-\d{2}$-\d{2}/;
-    
-
-    const errors = [];
-
-    if(!dateInicial){
-        errors.push("Preencha a data inicial")
-    }
-
-    if(!dateFinal){
-        errors.push("Preencha a data final")
-    }
-
-    if(pattern.test(dateInicial) || !isNaN(dateInicial)){
-        errors.push("Data Inicial invalida")
-    }
-
-    if(pattern.test(dateFinal) || !isNaN(dateFinal)){
-        errors.push("Data Final invalida")
-    }
-
-    if(errors.length!==0){
-       throw new AppError(errors[0], 401);
-    }
-
-    next();
-}
-
+import { ControllerAdapterExpress } from "../controllers/ControllerAdapterExpress";
+const controllerAdapterExpress = new ControllerAdapterExpress();
 
 transactionsRoutes.patch('/deposit', garantirAuth, (req: Request, res: Response)=>{
-    return depositTransactionsController.handle(req, res);
+    const controller = makeDepositTransactionsController();
+    return controllerAdapterExpress.handle(req, res, controller);
 });
 
 transactionsRoutes.patch('/withdraw', garantirAuth, (req: Request, res: Response)=>{
@@ -54,7 +25,7 @@ transactionsRoutes.patch('/sendingMoney', garantirAuth, (req: Request, res: Resp
     return sendingMoneyController.handle(req, res);
 });
 
-transactionsRoutes.get('/extracts', garantirAuth, validarDate, (req: Request, res: Response)=>{
+transactionsRoutes.get('/extracts', garantirAuth, (req: Request, res: Response)=>{
     return extractsByDataController.handle(req, res);
 });
 
