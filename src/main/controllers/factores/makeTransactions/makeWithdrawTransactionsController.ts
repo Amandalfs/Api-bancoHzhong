88d2e017115388115760c9d30fd841ca3bf6f90b
Amandalfs <amandalfs57@gmail.com>
2023-07-13 13:02:57 -0300

    feat: criado a fabrica de controlador de withdraw transactions

---
+++ src/main/controllers/factores/makeTransactions/makeWithdrawTransactionsController.ts
@@ -0,0 +1,12 @@
+import { ExtractsRepository } from "../../../../entities/repositories/knexRepositories/ExtractsRepository";
+import { UserRepository } from "../../../../entities/repositories/knexRepositories/UserRepository";
+import { WithdrawTransactionsController } from "../../../../presentations/controllers/transactions/WithdrawTransactions/WithdrawTransactionsController";
+import { WithdrawTransactionsUseCase } from "../../../../presentations/controllers/transactions/WithdrawTransactions/WithdrawTransactionsControllerProtocols";
+
+export function makeWithdrawTransactionsController(){
+    const userRepository = new UserRepository();
+    const extractsRepository = new ExtractsRepository();
+    const withdrawTransactionsUseCase = new WithdrawTransactionsUseCase(userRepository, extractsRepository);
+    const controller = new WithdrawTransactionsController(withdrawTransactionsUseCase);
+    return controller;
+}
