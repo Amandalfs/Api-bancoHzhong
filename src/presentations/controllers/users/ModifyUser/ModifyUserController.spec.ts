685aa636be956253a8fad075807702911434aaa9
Amandalfs <amandalfs57@gmail.com>
2023-09-01 18:04:06 -0300

    test: create error handling for status 401 in the modify user controller.

--- src/presentations/controllers/users/ModifyUser/ModifyUserController.spec.ts
+++ src/presentations/controllers/users/ModifyUser/ModifyUserController.spec.ts
@@ -63,4 +63,25 @@ describe("Modify user controller tests units", ()=>{
         expect(response.statusCode).toEqual(400);
         expect(response.body.msg).toEqual("Bad Request");
     })
+
+    it("should throw a 401 unauthorized error status", async ()=>{
+        const { sut, useCase } = makeSut();
+        vi.spyOn(useCase, "execute").mockRejectedValue({
+            statusCode: 401,
+            message: "Unauthorized"
+        });
+        const request: HttpRequest = {
+            body: {
+                username: "Error400",
+                name: "Error400Name", 
+                email: "Error400Email"
+            },
+            user: {
+                id: 1
+            }
+        }
+        const response = await sut.handle(request);
+        expect(response.statusCode).toEqual(401);
+        expect(response.body.msg).toEqual("Unauthorized");
+    })
 })
