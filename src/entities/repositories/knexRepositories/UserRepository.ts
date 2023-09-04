621cefdc425d082eba22ba83af6147c8474b537a
Amandalfs <amandalfs57@gmail.com>
2023-09-04 13:06:13 -0300

    fix: Fix the error of sending multiple updates to the repository at once instead of one.

--- src/entities/repositories/knexRepositories/UserRepository.ts
+++ src/entities/repositories/knexRepositories/UserRepository.ts
@@ -45,8 +45,8 @@ class UserRepository implements IUserRepository {
     }
     
     async updateAccont(id: number, user: IReponseUser): Promise<IReponseUser> {
-        const newUser = await db("users").where("id", id).update(user).returning("*").first();
-        return newUser
+        const newUser = await db("users").where("id", id).update(user).returning("*");
+        return newUser[0];
     }
 }
 
