11ac3477ee3c9f2d27bfa92016641f04e3e2c807
Amandalfs <amandalfs57@gmail.com>
2023-09-04 13:08:30 -0300

    feat: Create a route for modifying the user.

--- src/main/routes/users.routes.ts
+++ src/main/routes/users.routes.ts
@@ -11,20 +11,26 @@ const controllerAdapterExpress = new ControllerAdapterExpress;
 import { makeSessionsUserController } from "../controllers/factores/makeUsers/makeSessionsUserController";
 import { makeCreateUserController } from "../controllers/factores/makeUsers/makeCreateUserController";
 import { makeShowUserController } from "../controllers/factores/makeUsers/makeShowUserController";
+import { makeModifyUserController } from "../controllers/factores/makeUsers/makeModifyUserController";
 
-usersRoutes.post('/sessions', (req: Request, res: Response)=>{
+usersRoutes.post("/sessions", (req: Request, res: Response)=>{
     const controller = makeSessionsUserController();
     return controllerAdapterExpress.handle(req, res, controller);
 });
 
-usersRoutes.post('/', (req: Request, res: Response)=>{
+usersRoutes.post("/", (req: Request, res: Response)=>{
     const controller = makeCreateUserController();
     return controllerAdapterExpress.handle(req, res, controller);
 });
 
-usersRoutes.get('/show', garantirAuth, (req: Request, res: Response)=>{
+usersRoutes.get("/show", garantirAuth, (req: Request, res: Response)=>{
     const controller = makeShowUserController();
     return controllerAdapterExpress.handle(req, res, controller);
 });
+
+usersRoutes.patch("/modify", garantirAuth, (req: Request, res: Response)=>{
+    const controller = makeModifyUserController();
+    return controllerAdapterExpress.handle(req, res, controller);
+})
 
 export { usersRoutes };
