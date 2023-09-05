fb55b0ddc4c36cfba20ab48fd162fcfab58ba720
Amandalfs <amandalfs57@gmail.com>
2023-09-05 13:53:22 -0300

    test: Handle unknown errors in the user metrics controller and create controller metrics user.

---
+++ src/presentations/controllers/users/MetricsUser/MetricsUserController.spec.ts
@@ -0,0 +1,43 @@
+import { describe, expect, vi, it } from "vitest";
+import { IMetricsUserUseCase, InputMetricsUserUseCaseDTO, OutputMetricsUserUseCaseDTO } from "../../../../domain/module/acconts/useCase/MetricsUser/MetricsUserUseCase";
+import { HttpRequest } from "../CreateUser/CreateUserControllerProtocols";
+import { MetricsUserController } from "./MetricsUserController";
+
+interface TypeSuit {
+    useCase: IMetricsUserUseCase
+    suit: MetricsUserController
+};
+
+const makeSuit = (): TypeSuit => {
+    const useCase = new class UseCase implements IMetricsUserUseCase{
+        async execute({}: InputMetricsUserUseCaseDTO): Promise<OutputMetricsUserUseCaseDTO> {
+            return new Promise(resolve => resolve({
+                monthlyExpenses: 10,
+                monthlyIncome: 500,
+            }))
+        }
+    }
+    const suit = new MetricsUserController(useCase);
+    
+    return {
+        suit, 
+        useCase
+    }
+};
+
+describe("metrics user controller tests units", ()=>{
+
+    it("should throw a 500 server error status", async()=>{
+        const { suit, useCase } = makeSuit();
+
+        vi.spyOn(useCase, "execute").mockRejectedValue(new Error);
+        const request: HttpRequest = {
+            user: {
+                id: 1
+            }
+        }
+        const response = await suit.handle(request);
+        expect(response.statusCode).toEqual(500);
+        expect(response.body.msg).toEqual("Server Internal Error");
+    });
+});
