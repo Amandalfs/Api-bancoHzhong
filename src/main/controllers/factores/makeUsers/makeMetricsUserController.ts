7fdff9eb7c32432e603db2770f7c34b106e1fe54
Amandalfs <amandalfs57@gmail.com>
2023-09-05 16:08:10 -0300

    feat: Create the user metrics controller factory.

---
+++ src/main/controllers/factores/makeUsers/makeMetricsUserController.ts
@@ -0,0 +1,13 @@
+
+import { MetricsUserUseCase } from '../../../../domain/module/acconts/useCase/MetricsUser/MetricsUserUseCase';
+import { ExtractsRepository } from '../../../../entities/repositories/knexRepositories/ExtractsRepository';
+import { UserRepository } from '../../../../entities/repositories/knexRepositories/UserRepository';
+import { MetricsUserController } from './../../../../presentations/controllers/users/MetricsUser/MetricsUserController';
+
+export function makeMetricsUserController(): MetricsUserController {
+    const usersRepository = new UserRepository();
+    const extractsRepository = new ExtractsRepository();
+    const useCase = new MetricsUserUseCase(usersRepository, extractsRepository);
+    const controller = new MetricsUserController(useCase);
+    return controller;
+}
