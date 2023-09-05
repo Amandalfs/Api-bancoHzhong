8926815d409bd73083712dcb5cb2245e5d42ad3a
Amandalfs <amandalfs57@gmail.com>
2023-09-05 13:30:07 -0300

    feat: Add the functions for incomes and expenses to the Knex extract repository.

--- src/entities/repositories/knexRepositories/ExtractsRepository.ts
+++ src/entities/repositories/knexRepositories/ExtractsRepository.ts
@@ -3,11 +3,13 @@ import { IExtracsRepository, IRequestCountBySending, IRequestCountByWithdraw, IR
 import {IExtracts} from "../model/IExtracts";
 
 class ExtractsRepository implements IExtracsRepository {
+    
     async SearchForMoreRecentExtractsById(id_user: number){
-        return await db('extratos')
+        const extracts = await db('extratos')
         .select("tipo", "saldo", "data", "descricao")
         .where("id_user",id_user).orderBy('data', 'desc')
         .limit(5);
+        return extracts;
     }
 
     async createExtracts(data: IExtracts){
@@ -15,29 +17,62 @@ class ExtractsRepository implements IExtracsRepository {
     }
 
     async SearchForDataStartAndEndbyId({id, dateStart, dateEnd}: IRequestSearchForDataStartAndEndbyId){
-        return await db('extratos')
+        const extracts = await db('extratos')
         .select("tipo", "saldo", "data", "descricao")
         .where("id_user",id).where('data', '>=', dateStart)
         .where('data', '<=', dateEnd);
+
+        return extracts;
     }
 
     async CountByWithdraw({dateStart, dateEnd, UserId}: IRequestCountByWithdraw): Promise<number> {
-        return await db("extratos")
+        const count: number = await db("extratos")
         .where("id_user", UserId)
         .where('tipo','saque')
         .where('data', '>=', dateStart)
         .where('data', '<=', dateEnd)
         .count("saldo");
+
+        return count;
     }
 
     async CountBySending({dateStart, dateEnd, UserId }: IRequestCountBySending): Promise<number> {
-        return await db("extratos")
+        const count: number = await db("extratos")
         .where("id_user", UserId)
         .where('tipo','envio')
         .where('data', '>=', dateStart)
         .where('data', '<=', dateEnd)
         .count("saldo");
+
+        return count;
+    }
+
+    async findIncomesByDate({ id, lastMonth, today }: { id: number; today: Date; lastMonth: Date; }): Promise<number> {
+        const count: number = await db("extratos")
+        .where("id_user", id)
+        .where((builder) => {
+            builder.where("tipo", "recebido").orWhere("tipo", "deposito");
+        })
+        .where('data', '>=', lastMonth)
+        .where('data', '<=', today)
+        .count("saldo");
+
+        return count;
+    }
+
+    async findExpensesByDate({ id, lastMonth, today }: { id: number; today: Date; lastMonth: Date; }): Promise<number> {
+        const count: number = await db("extratos")
+        .where("id_user", id)
+        .where('data', '>=', lastMonth)
+        .where('data', '<=', today)
+        .orWhere("tipo","envio")
+        .where((builder) => {
+            builder.where("tipo", "envio").orWhere("tipo", "Saque");
+        })
+        .count("saldo");
+
+        return count;
     }
 }
 
-export  {ExtractsRepository};
+export  { ExtractsRepository };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, UserConfigExport } from "vitest/config";
+interface KnexConfig {
+	environment: string[][];
+	exclude: string[];
+	setTimeout: number;
+}
 
-export default defineConfig({
+interface ExtendedConfig extends UserConfigExport {
+	knex: KnexConfig;
+}
+export default defineConfig<ExtendedConfig>({
 	test: {
 		environmentMatchGlobs:[["src/**", ""]],	
 		exclude:['node_modules', 'dist', '.idea', '.git', '.cache', 'src/main/**']
