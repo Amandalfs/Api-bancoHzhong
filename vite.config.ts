4e41551de806c30047edd3307ba566f802fa80bf
Amandalfs <amandalfs57@gmail.com>
2023-09-06 13:38:19 -0300

    chore: create file config test e2e

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from "vitest/config";
+const { defineConfig } = require("vitest/config");
 
 export default defineConfig({
 	test: {
 		environmentMatchGlobs:[["src/**", ""]],	
 		exclude:['node_modules', 'dist', '.idea', '.git', '.cache', 'src/main/**']
-	},
-	knex: {
-		environment: [["src/**", "knex"]],
-		exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
-		setTimeout: 10000
 	}
 });
