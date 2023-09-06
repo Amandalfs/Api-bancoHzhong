7c9ff59453980658527a8a9e3022a815fab1db5a
Amandalfs <amandalfs57@gmail.com>
2023-09-06 14:34:05 -0300

    test: Create a test validating that the use case of the 'grafic extracs pizza' controller is receiving the data correctly.

--- src/presentations/controllers/transactions/GrafictExtractsPizza/GraficExtractsPizzaController.spec.ts
+++ src/presentations/controllers/transactions/GrafictExtractsPizza/GraficExtractsPizzaController.spec.ts
@@ -117,4 +117,26 @@ describe("grafic extracs pizza controller tests units", ()=>{
         expect(response.statusCode).toEqual(404);
         expect(response.body.msg).toEqual("Not Found");
     });
+
+    it("should send the data correctly to the use case.", async ()=>{
+        const { suit, useCase } = makeSuit();
+        const useCaseSpy = vi.spyOn(useCase, "execute");
+        
+        const request: HttpRequest = {
+            user: {
+                id: 1
+            },
+            query: {
+                startDate: new Date(),
+                endDate: new Date(),
+            }
+        }
+        
+        await suit.handle(request);
+        expect(useCaseSpy.mock.calls[0][0]).toEqual({
+            id: request.user.id,
+            startDate: request.query.startDate,
+            endDate: request.query.endDate,
+        });
+    });
 })
