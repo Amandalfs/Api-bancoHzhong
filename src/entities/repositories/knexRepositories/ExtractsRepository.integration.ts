5cbce46fb2cdf051dc83bfd16d7b2ecca6e0e602
Amandalfs <amandalfs57@gmail.com>
2023-09-11 13:23:17 -0300

    test: create an integration test for the expensesExtractsByDays function of the extracts repository.

--- src/entities/repositories/knexRepositories/ExtractsRepository.integration.ts
+++ src/entities/repositories/knexRepositories/ExtractsRepository.integration.ts
@@ -315,4 +315,63 @@ describe("repository extracts tests integrations  by knex", () => {
         expect(total).toEqual(1500);
 
     })
+
+    it("should be able to search for an array with a list divided by day within the interval with the date and value.", async ()=>{
+        
+        vi.setSystemTime(new Date(2023, 5, 1));
+
+        const [{ id }] = await db("users").insert({
+            name: "Usuario Test",
+            username: "UsuarioTest",
+            nasc: "02-10-2003",
+            typeaccont: "poupanca",
+            password: "12345678",
+            cpf: "12603863096",
+            agencia: "123",
+            saldo: 50,
+            keypix: null,
+        }).returning("id");
+
+        const schemaExtract = {
+            data: new Date(2023, 1, 1),
+            descricao: "test",
+            id_user: id,
+            name: "Test",
+            saldo: 150,
+            tipo: "Saque",
+        }
+
+        await db("extratos").insert(schemaExtract);
+        
+        for (let index = 1; index <= 10; index++) {
+            await db("extratos").insert({
+                data: new Date(2023, 4, index, 1),
+                descricao: "test",
+                id_user: id,
+                name: "Test",
+                saldo: 150,
+                tipo: index<5?"saque":"envio",
+            });
+            await db("extratos").insert({
+                data: new Date(2023, 4, index, 2),
+                descricao: "test",
+                id_user: id,
+                name: "Test",
+                saldo: 150,
+                tipo: index<5?"saque":"envio",
+            });
+        }
+
+        const extractsGrafic = await new ExtractsRepository().expensesExtractsByDays({
+            startDate: new Date(2023, 4, 1),
+            endDate: new Date(2023, 5, 1),
+            userId: id,
+        })
+
+        expect(extractsGrafic).toHaveLength(10);
+        expect(extractsGrafic[0]).toEqual({
+            date: new Date(2023, 4, 1),
+            value: 300,
+        })
+    })
 })
