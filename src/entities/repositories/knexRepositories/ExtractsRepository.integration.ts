fe618a5c5a6d66fae70214c1ef6841e481ae1105
Amandalfs <amandalfs57@gmail.com>
2023-09-11 12:00:20 -0300

    test: create an integration test for the SearchForDataStartAndEndbyId function of the extracts repository.

--- src/entities/repositories/knexRepositories/ExtractsRepository.integration.ts
+++ src/entities/repositories/knexRepositories/ExtractsRepository.integration.ts
@@ -78,4 +78,55 @@ describe("repository extracts tests integrations  by knex", () => {
             descricao: 'test'
           })
     });
+
+    it("should be able to search for all extracts within a time period.", async ()=>{
+        vi.setSystemTime(new Date(2023, 5, 1));
+
+        const [{ id }] = await db("users").insert({
+            name: "Usuario Test",
+            username: "UsuarioTest",
+            nasc: "02-10-2003",
+            typeaccont: "poupanca",
+            password: "12345678",
+            cpf: "12603863096",
+            agencia: "123",
+            saldo: 50,
+            keypix: null,
+        }).returning("id");
+
+        const schemaExtract = {
+            data: new Date(2023, 1, 1),
+            descricao: "test",
+            id_user: id,
+            name: "Test",
+            saldo: 150,
+            tipo: "Saque",
+        }
+
+        await db("extratos").insert(schemaExtract);
+        
+        for (let index = 1; index <= 10; index++) {
+            await db("extratos").insert({
+                data: new Date(2023, 4, index),
+                descricao: "test",
+                id_user: id,
+                name: "Test",
+                saldo: 150,
+                tipo: "Saque",
+            });
+        }
+
+        const extracts = await new ExtractsRepository().SearchForDataStartAndEndbyId({
+            id,
+            dateStart: new Date(2023, 4, 1),
+            dateEnd: new Date(2023, 5, 1),
+        })
+        expect(extracts).toHaveLength(10);
+        expect(extracts[0]).toEqual({
+            tipo: 'Saque',
+            saldo: 150,
+            data: new Date(2023, 4, 1),
+            descricao: 'test'
+        })
+    })
 })
