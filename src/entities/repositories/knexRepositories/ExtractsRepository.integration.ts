e8d830f0063f650ecf996358f80ca9482bae980e
Amandalfs <amandalfs57@gmail.com>
2023-09-12 11:38:46 -0300

    test: Create a function to return the total number of documents based on the date in the extracts repository.

--- src/entities/repositories/knexRepositories/ExtractsRepository.integration.ts
+++ src/entities/repositories/knexRepositories/ExtractsRepository.integration.ts
@@ -438,4 +438,50 @@ describe("repository extracts tests integrations  by knex", () => {
         })
 
     })
+
+    it("Should be able to count the total documents from the query.", async () => {
+        vi.setSystemTime(new Date(2023, 5, 1));
+
+        const [{ id }] = await db("users").insert({
+            name: "Usuario Test",
+            username: "UsuarioTest",
+            nasc: "02-10-2003",
+            typeaccont: "poupanca",
+            password: "12345678",
+            cpf: "12603863096",
+            agencia: "123",
+            saldo: 50,
+            keypix: null,
+        }).returning("id");
+
+        const schemaExtract = {
+            data: new Date(2023, 1, 1),
+            descricao: "test",
+            id_user: id,
+            name: "Test",
+            saldo: 150,
+            tipo: "Saque",
+        }
+
+        await db("extratos").insert(schemaExtract);
+        
+        for (let index = 1; index <= 10; index++) {
+            await db("extratos").insert({
+                data: new Date(2023, 4, index),
+                descricao: "test",
+                id_user: id,
+                name: "Test",
+                saldo: 150,
+                tipo: "Saque",
+            });
+        }
+
+        const total = await new ExtractsRepository().getCountDocs({
+            userId: id,
+            startDate: new Date(2023, 4, 1),
+            endDate: new Date(2023, 5, 1),
+        })
+
+        expect(total).toEqual(10);
+    });
 })
