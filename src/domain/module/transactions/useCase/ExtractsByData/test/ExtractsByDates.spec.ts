4276072af53a277b92db76cdd0e617cfeb91ece1
Amandalfs <amandalfs57@gmail.com>
2023-09-12 11:14:20 -0300

    test: Add pagination and pagination details to the extracts use case.

--- src/domain/module/transactions/useCase/ExtractsByData/test/ExtractsByDates.spec.ts
+++ src/domain/module/transactions/useCase/ExtractsByData/test/ExtractsByDates.spec.ts
@@ -12,7 +12,7 @@ describe("testando useCase de estrato", ()=>{
 
     beforeEach(()=>{
         usersRepository = new InMemoryUsersRepository;
-        extractsRepository = new InMemoryExtractsRepository;
+        extractsRepository = new InMemoryExtractsRepository();
         sut = new ExtractsByDataUseCase(extractsRepository);
         vi.useFakeTimers();
     })
@@ -46,28 +46,31 @@ describe("testando useCase de estrato", ()=>{
             name: "Usuario Test",
             tipo: "deposito",
             saldo: 50,
-            data: `${new Date()}`,
+            data: new Date(),
             descricao: "",
         })
 
         vi.setSystemTime(new Date("02-02-2023"));
 
-        for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 10; index++) {
             await extractsRepository.createExtracts({
                 id_user: 1,
                 name: "Usuario Test",
                 tipo: "deposito",
                 saldo: 50,
-                data: `${new Date()}`,
+                data: new Date(),
                 descricao: "",
             })
             
         }
   
-        const id_user = 1, dateStart = "02-02-2023", dateEnd = "02-04-2023";
+        const id_user = 1, dateStart = new Date("02-02-2023"), dateEnd = new Date("02-04-2023");
 
-        const { extracts } = await sut.execute({id_user, dateStart, dateEnd});
+        const { extracts, details } = await sut.execute({id_user, dateStart, dateEnd, page: 1, rows: 5});
         expect(extracts).toEqual(expect.any(Array))
-        expect(extracts).toHaveLength(5);
+        expect(extracts).toBeDefined();
+        expect(details.totalDocs).toEqual(10);
+        expect(details.page).toEqual(1);
+        expect(details.pagesTotal).toEqual(2);
     })
 })
